82288cb9bdaacae6f6e59a42530c6ad87ed520d9
kskim0119 <dooly9931@snu.ac.kr>
2022-01-04 21:50:38 +0900

    eslint configuration

--- myapp/src/routes/index.js
+++ myapp/src/routes/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 var router = express.Router();
 
 /* GET home page. */
+// eslint-disable-next-line no-unused-vars
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
 });
